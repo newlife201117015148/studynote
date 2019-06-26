3417468832a60a7dd8a2ce3fadd6545128ff5b46
wl <527624087@qq.com>
2019-06-26 23:09:04 +0800

    更改第一次

--- src/com/wangle/Test1.java
+++ src/com/wangle/Test1.java
@@ -17,88 +17,21 @@ public class Test1 {
 	 * @throws ：
 	 */
 	public static void main(String[] args) {
-//		int[] a = { 1, 1, 1, 2, 2, 2, 3, 3, 3, 4 };
-//		singleNumberII(a);
-		
-//		System.out.println(findNums(new int[]{12,23,1,1,2,2,3,3}));
-		MyABS(-12312412);
+		 int[] a = { 1, 31, 21, 21, 2, 24, 3, 33, 3, 4 };
+		 
+		 for(int i=0;i<a.length;i++){
+			 for(int j=i+1;j<a.length;j++){
+				 if(a[j]<a[i]){
+					 int temp = a[j];
+					 a[j] = a[i];
+					 a[i] = temp;
+				 }
+			 }
+		 }
+		 
+		 for (int i : a) {
+			System.out.println(i);
+		}
 	}
 
-	
-	public static char byteToChar(byte[] b) {
-		char c = (char) (((b[0] & 0xFF) << 8) | (b[1] & 0xFF));
-		return c;
-	}
-
-	public static int singleNumberII(int[] A) {
-		if (A == null || A.length == 0) {
-			return 0;
-		}
-		int result = 0;
-		int a[] = new int[32];
-		for (int i = 0; i < 32; i++) {
-			for (int j = 0; j < A.length; j++) {
-				a[i] += A[j] >> i & 1;
-			}
-			a[i] %= 3;
-			result = result | (a[i] << i);
-		}
-		return result;
-	}
-
-	// 取二进制中首个为1的位置
-	public static int findFirstOne(int value) {
-		int pos = 0;
-		while ((value & 1) != 1) {
-			value = value >> 1;
-			pos++;
-		}
-		return pos;
-	}
-
-	// 测试某位置是否为1
-	public static boolean testBit(int value, int pos) {
-		return ((value >> pos) & 1) == 1 ? true : false;
-	}
-	//一个整数数组里面，除了两个数之外，其他的数字都出现了两次，写一个程序找出这两个数
-	public static String  findNums(int date[]) {
-		int num1  = 0;
-		int num2  = 0;
-		int length = date.length;
-		if (length < 2) {
-			return "-1";
-		}
-
-		int ansXor = 0;
-		int i = 0;
-		int pos = 0;
-
-		for (i = 0; i < length; i++) {
-			ansXor ^= date[i]; // 异或
-		}
-
-		pos = findFirstOne(ansXor);
-
-		
-		for (i = 0; i < length; i++) {
-			if (testBit(date[i], pos)) {
-				num1 ^= date[i];
-			} else {
-				num2 ^= date[i];
-			}
-		}
-
-		return num1 +"\t"+num2;
-
-	}
-	
-	public static void MyABS(int fushu){
-		if(fushu>=0){
-			JOptionPane.showMessageDialog(null, "请输入负数");
-			return;
-		}
-		
-		fushu = (fushu ^ -1)+1;
-		System.out.println(fushu);
-	}
 }
